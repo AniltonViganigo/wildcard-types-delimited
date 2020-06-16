98b2d1b4038561c48d1b2de38029d1ad3b429f8f
Anilton Viganigo Junior <viganigojr@gmail.com>
2020-06-15 22:41:14 -0300

    Incluindo o tipo curinga delimitado no método totalArea

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,11 +14,15 @@ public class Program {
 		List<Shape> myShapes = new ArrayList<>();
 		myShapes.add(new entities.Rectangle(3.0, 2.0));
 		myShapes.add(new Circle(2.0));
+		
+		List<Circle> myCircle = new ArrayList<>();
+		myCircle.add(new Circle(3.0));
+		myCircle.add(new Circle(4.0));
 
-		System.out.println("Área total: " + String.format("%.2f", totalArea(myShapes)));
+		System.out.println("Área total: " + String.format("%.2f", totalArea(myCircle)));
 	}
 
-	public static double totalArea(List<Shape> list) {
+	public static double totalArea(List<? extends Shape> list) {
 		double soma = 0.0;
 		for (Shape s : list) {
 			soma += s.area();
